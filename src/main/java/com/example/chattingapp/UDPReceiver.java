798e227ab54d92cd1d6f0fe59d76a27fd9b0c9c6
s-aljufayr <123065236+s-aljufayr@users.noreply.github.com>
2023-06-15 04:37:45 +0300

    break loop sending tracks if radar.isStatus false part of UDP Receiver

--- src/main/java/com/example/chattingapp/UDPReceiver.java
+++ src/main/java/com/example/chattingapp/UDPReceiver.java
@@ -1,8 +1,19 @@
 package com.example.chattingapp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class UDPReceiver {
     private static int receiverPort = 9002;
+    private boolean isReceivingMessages = true;
 
     public UDPReceiver() {
+    }
+    private void startReceiving() throws SocketException {
+        DatagramSocket socket = new DatagramSocket(receiverPort);
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer,1024);
+
     }
 }
